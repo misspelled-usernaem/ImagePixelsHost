7f0ba883b212dc857f058ea02f95edbf96e473c5
misspelled-usernaem <134952931+misspelled-usernaem@users.noreply.github.com>
2023-10-05 18:52:16 +0200

    rrr

--- app.py
+++ app.py
@@ -5,38 +5,32 @@ from time import sleep as wait
 import cv2 as cv
 from datetime import datetime
 
-cached_urls={}
-
-def main(img_url,size):
-    if cached_urls.get(img_url):
-        return cached_urls[img_url]
-    else:
-        img_content=requests.get(img_url).content
-        img_name=str(datetime.now().replace(microsecond=0)).replace(":","-")+'.png'
-        with open(f'./{img_name}','wb') as f:
-            f.write(img_content)
-        ssDir=os.path.join('./',img_name)
-        image=cv.imread(ssDir,cv.IMREAD_UNCHANGED)
-        image=cv.cvtColor(image,cv.COLOR_BGR2BGRA)
-        currentSize=image.shape
-        newSize=[currentSize[1]>255 and 255 or currentSize[1],currentSize[0]>255 and 255 or currentSize[0]]
-        if size:
-            newSize=size
-        image=cv.resize(image,newSize)
-        currentSize=image.shape
-        fullList=[]
-        for colum in range(currentSize[0]):
-            crow=[]
-            for row in range(currentSize[1]):
-                BGRA=image[row,colum]
-                RGB={"R":BGRA[2],"G":BGRA[1],"B":BGRA[0],"A":BGRA[3]}
-                crow.append(RGB)
-            fullList.append(str(crow))
-        jsonList='[\n\t'+str.join(',\n\t',fullList)+'\n]'
-        jsonList=jsonList.replace("'",'"')
-        source='[\n\t'+str(fullList).replace('"','').replace("'",'"').replace('[{','\n\t\t[{').replace('}, {','},{').replace(', "',',"').replace('": ','":').replace(']]',']\n\t]')+'\n]'
-        os.remove(ssDir)
-        return source
+def main(img_url):
+    img_content=requests.get(img_url).content
+    img_name=str(datetime.now().replace(microsecond=0)).replace(":","-")+'.png'
+    with open(f'./{img_name}','wb') as f:
+        f.write(img_content)
+    ssDir=os.path.join('./',img_name)
+    image=cv.imread(ssDir,cv.IMREAD_UNCHANGED)
+    image=cv.cvtColor(image,cv.COLOR_BGR2BGRA)
+    currentSize=image.shape
+    newSize=[currentSize[1]>255 and 255 or currentSize[1],currentSize[0]>255 and 255 or currentSize[0]]
+   
+    image=cv.resize(image,newSize)
+    currentSize=image.shape
+    fullList=[]
+    for colum in range(currentSize[0]):
+        crow=[]
+        for row in range(currentSize[1]):
+            BGRA=image[colum,row]
+            RGB={"R":BGRA[2],"G":BGRA[1],"B":BGRA[0],"A":BGRA[3]}
+            crow.append(RGB)
+        fullList.append(str(crow))
+    jsonList='[\n\t'+str.join(',\n\t',fullList)+'\n]'
+    jsonList=jsonList.replace("'",'"')
+    source='[\n\t'+str(fullList).replace('"','').replace("'",'"').replace('[{','\n\t\t[{').replace('}, {','},{').replace(', "',',"').replace('": ','":').replace(']]',']\n\t]')+'\n]'
+    os.remove(ssDir)
+    return source
 
 
 app=Flask(__name__,)
@@ -49,14 +43,12 @@ def idata():
     if payload.get('url'):
         try:
             img_url=payload['url']
-            size=False
-            if payload.get('x') and payload.get('y'):
-                size=[int(payload['x']),int(payload['y'])]
             print(img_url)
-            data=main(img_url,size=size)
+            data=main(img_url)
             return data
         except:
-            return 'invalid link'
+            return 'an error occured'
+        
     else:
         return 'missing key in payload'
 
@@ -69,11 +61,8 @@ def bulk():
             for urlDesc in payload['urls']:
                 try:
                     img_url=urlDesc.get('url')
-                    size=False
-                    if urlDesc.get('x') and urlDesc.get('y'):
-                        size=[int(urlDesc['x']),int(urlDesc['y'])]
                     print(img_url)
-                    data=main(img_url,size=size)
+                    data=main(img_url)
                     returnValue.append(data)
                 except:
                     print('[ERROR] Could not deconsturct image')
